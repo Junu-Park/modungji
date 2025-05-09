5ad69aa8dd478f23fe3a8dbeac5c3961d35c51f5
Park Junwoo <ays99111@naver.com>
2025-05-09 17:23:33 +0900

    xcode 프로젝트 파일 추가

---
+++ Modungji/ModungjiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModungjiApp.swift
+//  Modungji
+//
+//  Created by 박준우 on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ModungjiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
