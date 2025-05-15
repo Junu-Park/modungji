1384c63217d8535989e501f502d61652b4006895
Park Junwoo <ays99111@naver.com>
2025-05-15 20:32:52 +0900

    EstateRouter 파일 추가
    Estate Router 구현

---
+++ Modungji/Core/Network/Router/Estate/EstateRouter.swift
@@ -0,0 +1,15 @@
+//
+//  EstateRouter.swift
+//  Modungji
+//
+//  Created by 박준우 on 5/15/25.
+//
+
+import Foundation
+
+import Alamofire
+import EstateAPI
+
+enum EstateRouter {
+    static let baseURL: String = EstateAPI.baseURL
+}
