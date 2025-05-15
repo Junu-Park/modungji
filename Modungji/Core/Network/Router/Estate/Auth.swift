3a3c8f6ec6429ba35a9246a9a928b1e9257d10f4
Park Junwoo <ays99111@naver.com>
2025-05-15 20:36:18 +0900

    Auth 파일 추가
    Estate API Auth 라우터 구현

---
+++ Modungji/Core/Network/Router/Estate/Auth.swift
@@ -0,0 +1,61 @@
+//
+//  Auth.swift
+//  Modungji
+//
+//  Created by 박준우 on 5/15/25.
+//
+
+import Foundation
+
+import Alamofire
+import EstateAPI
+
+extension EstateRouter {
+    enum Auth: APIRouter {
+        case renewToken
+        
+        var baseURL: URL? {
+            return URL(string: EstateRouter.baseURL)?.appendingPathComponent("v1/auth")
+        }
+        
+        var path: String {
+            switch self {
+            case .renewToken:
+                return "refresh"
+            }
+        }
+        
+        var method: HTTPMethod {
+            switch self {
+            case .renewToken:
+                return .get
+            }
+        }
+        
+        var queryParameters: [String : Any]? {
+            switch self {
+            case .renewToken:
+                return nil
+            }
+        }
+        
+        var body: Encodable? {
+            switch self {
+            case .renewToken:
+                return nil
+            }
+        }
+        
+        var headers: HTTPHeaders {
+            var headers = HTTPHeaders()
+            headers.add(name: "SeSACKey", value: EstateAPI.apiKey)
+            
+            switch self {
+            case .renewToken:
+                headers.add(name: "RefreshToken", value: "")
+            }
+            
+            return headers
+        }
+    }
+}
