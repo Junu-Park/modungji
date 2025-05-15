e09d405b7968d86ef044e3c7bc8953b8defbd006
Park Junwoo <ays99111@naver.com>
2025-05-15 21:51:47 +0900

    ValidateEmail API DTO 파일 추가
    Request, Response DTO 파일 구현

---
+++ Modungji/Core/Network/DTO/Response/ValidateEmailResponseDTO.swift
@@ -0,0 +1,12 @@
+//
+//  ValidateEmailResponseDTO.swift
+//  Modungji
+//
+//  Created by 박준우 on 5/15/25.
+//
+
+import Foundation
+
+struct ValidateEmailResponseDTO: Decodable {
+    let message: String
+}
