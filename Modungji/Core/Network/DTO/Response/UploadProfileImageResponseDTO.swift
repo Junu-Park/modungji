ce308d38b329f430622e9ecd7ce320628a6e0ece
Park Junwoo <ays99111@naver.com>
2025-05-15 22:04:44 +0900

    Upload Profile Image API Response DTO 파일 추가

---
+++ Modungji/Core/Network/DTO/Response/UploadProfileImageResponseDTO.swift
@@ -0,0 +1,12 @@
+//
+//  UploadProfileImageResponseDTO.swift
+//  Modungji
+//
+//  Created by 박준우 on 5/15/25.
+//
+
+import Foundation
+
+struct UploadProfileImageResponseDTO: Decodable {
+    let profileImage: String
+}
