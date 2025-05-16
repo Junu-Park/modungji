f7dc8c865fc504b62c59aa49248d9904b27af72a
Park Junwoo <ays99111@naver.com>
2025-05-16 21:23:05 +0900

    ModungjiApp 파일 수정
    카카오 로그인 리다이렉트 로직 추가

--- Modungji/ModungjiApp.swift
+++ Modungji/ModungjiApp.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 import EstateAPI
+import KakaoSDKAuth
 import KakaoSDKCommon
 
 @main
@@ -20,6 +21,16 @@ struct ModungjiApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onOpenURL { url in
+                    if AuthApi.isKakaoTalkLoginUrl(url) {
+                        guard AuthController.handleOpenUrl(url: url) else {
+                            print("유효하지 않은 리다이렉트 URL")
+                            return
+                        }
+                    } else {
+                        print("카카오톡이 아닌 다른 곳에서 리다이렉트된 URL")
+                    }
+                }
         }
     }
 }
