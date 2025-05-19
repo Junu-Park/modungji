df5c24e1ae6138684afd7a71c30dc1bdd8e9f30e
Park Junwoo <ays99111@naver.com>
2025-05-19 16:09:08 +0900

    ValidateEmailResponseEntity 파일 생성
    ValidateEmailResponseDTO에 대응하는 Entity 구현

---
+++ Modungji/Feature/Auth/Model/ValidateEmailResponseEntity.swift
@@ -0,0 +1,13 @@
+//
+//  ValidateEmailResponseEntity.swift
+//  Modungji
+//
+//  Created by 박준우 on 5/19/25.
+//
+
+import Foundation
+
+struct ValidateEmailResponseEntity {
+    let isValid: Bool
+    let message: String 
+}
