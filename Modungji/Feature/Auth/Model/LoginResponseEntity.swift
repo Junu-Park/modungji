d4c552ebb7d337fe04406d771722606ec5d650a8
Park Junwoo <ays99111@naver.com>
2025-05-19 16:18:32 +0900

    LoginResponseEntity 파일 추가
    LoginResponseDTO에 대응하는 Entity 구현

---
+++ Modungji/Feature/Auth/Model/LoginResponseEntity.swift
@@ -0,0 +1,17 @@
+//
+//  LoginResponseEntity.swift
+//  Modungji
+//
+//  Created by 박준우 on 5/19/25.
+//
+
+import Foundation
+
+struct LoginResponseEntity {
+    let user_id: String
+    let email: String
+    let nick: String
+    let profileImage: String?
+    let accessToken: String
+    let refreshToken: String
+}
