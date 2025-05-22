cb8114a29e844480883bf8e2346ba2d494dfc143
Park Junwoo <ays99111@naver.com>
2025-05-22 20:28:13 +0900

    PasswordTextFieldView 파일 제거
    AuthWithEmailView 파일에 PasswordTextFieldView 파일 통합

--- Modungji/Feature/Auth/View/AuthWithEmailView.swift
+++ Modungji/Feature/Auth/View/AuthWithEmailView.swift
@@ -19,16 +19,16 @@ struct AuthWithEmailView: View {
     
     var body: some View {
         VStack(alignment: .leading) {
-            emailInputView()
+            self.emailInputView()
             
             if self.isSignUp {
-                nicknameInputView
+                self.nicknameInputView
             }
 
-            passwordInputView()
+            self.passwordInputView()
                 
             if self.isSignUp {
-                passwordCheckInputView
+                self.passwordCheckInputView
             }
             
             Spacer()
@@ -126,5 +126,48 @@ extension AuthWithEmailView {
                 }
         }
         .disabled(!self.viewModel.state.canConfirmAuthWithEmail)
+    }
+}
+
+// MARK: - PasswordTextFieldView
+private struct PasswordTextFieldView: View {
+    @EnvironmentObject var viewModel: AuthViewModel
+    @State private var isSecure: Bool = true
+    
+    private let isPasswordCheck: Bool
+    private var placeholder: String {
+        return self.isPasswordCheck ? "비밀번호 확인" : "비밀번호"
+    }
+    private var secureImage: String {
+        return self.isSecure ? "eye.slash" : "eye"
+    }
+    
+    init(isPasswordCheck: Bool = false) {
+        self.isPasswordCheck = isPasswordCheck
+    }
+    
+    var body: some View {
+        HStack(spacing: 4) {
+            if self.isSecure {
+                SecureField(self.placeholder, text: isPasswordCheck ? self.$viewModel.input.passwordCheck : self.$viewModel.input.password)
+            } else {
+                TextField(self.placeholder, text: isPasswordCheck ? self.$viewModel.input.passwordCheck : self.$viewModel.input.password)
+            }
+            
+            self.secureButton
+        }
+        .keyboardType(.alphabet)
+    }
+}
+
+// MARK: - PasswordTextFieldView Component
+extension PasswordTextFieldView {
+    var secureButton: some View {
+        Button {
+            self.isSecure.toggle()
+        } label: {
+            Image(systemName: self.secureImage)
+                .frame(width: 30, height: 30)
+        }
     }
 }
