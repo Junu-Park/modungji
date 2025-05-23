9c31b0900f2e816b92b1542d3bf2a231018d0319
Park Junwoo <ays99111@naver.com>
2025-05-23 15:59:26 +0900

    AuthWithEmailViewModel 파일 수정
    이메일 로그인/회원가입 로직 구현

--- Modungji/Feature/Auth/ViewModel/AuthWithEmailViewModel.swift
+++ Modungji/Feature/Auth/ViewModel/AuthWithEmailViewModel.swift
@@ -17,17 +17,23 @@ final class AuthWithEmailViewModel: ObservableObject {
     }
     
     struct State {
-        var emailValidationMessage: String = ""
+        var emailValidationMessage: String = "이메일을 입력해주세요"
         var isEmailValid: Bool = false
         var isNicknameValid: Bool = false
         var isPasswordValid: Bool = false
         var isMatchPassword: Bool = false
         var canAuth: Bool = false
+        var authType: AuthWithEmailType = .login
+        var loginData: LoginResponseEntity?
+        var showAlert: Bool = false
+        var alertMessage: String = ""
     }
 
     enum Action {
+        case changeAuthType(_ type: AuthWithEmailType)
         case login
         case signUp
+        case reset
     }
     
     @Published var state: State = .init()
@@ -42,22 +48,168 @@ final class AuthWithEmailViewModel: ObservableObject {
     
     func transform() {
         
+        self.$input.map(\.email)
+            .removeDuplicates()
+            .filter { [weak self] email in
+                guard let self, self.state.authType == .signUp else {
+                    return false
+                }
+                
+                // 이메일을 수정하는 동안, 버튼 비활성화
+                self.state.canAuth = false
+                
+                return true
+            }
+            .debounce(for: .milliseconds(300), scheduler: RunLoop.main)
+            .map { [weak self] email -> AnyPublisher<ValidateEmailResponseEntity, Never> in
+                guard let self else {
+                    return Just(ValidateEmailResponseEntity(isValid: false, message: "잠시 후, 다시 시도해주세요."))
+                        .eraseToAnyPublisher()
+                }
+                
+                return Future { [weak self] promise in
+                    guard let self else {
+                        return promise(.success(ValidateEmailResponseEntity(isValid: false, message: "잠시 후, 다시 시도해주세요.")))
+                    }
+                    
+                    Task {
+                        do {
+                            let result = try await self.service.checkEmailValidation(email)
+                            promise(.success(result))
+                        } catch {
+                            promise(.success(ValidateEmailResponseEntity(isValid: false, message: "잠시 후, 다시 시도해주세요.")))
+                        }
+                    }
+                }
+                .receive(on: DispatchQueue.main)
+                .eraseToAnyPublisher()
+            }
+            .switchToLatest()
+            .sink { [weak self] entity in
+                guard let self else {
+                    return
+                }
+                self.state.emailValidationMessage = entity.message
+                self.state.isEmailValid = entity.isValid
+            }
+            .store(in: &self.cancellables)
+        
+        self.$input
+            .map(\.nickname)
+            .removeDuplicates()
+            .sink { [weak self] nickname in
+                guard let self else {
+                    return
+                }
+                
+                self.state.isNicknameValid = !nickname.isEmpty
+            }
+            .store(in: &self.cancellables)
+        
+        Publishers.CombineLatest(self.$input.map(\.password), self.$input.map(\.passwordCheck))
+            .removeDuplicates(by: ==)
+            .sink { [weak self] password, passwordCheck in
+                guard let self else {
+                    return
+                }
+                
+                self.state.isPasswordValid = self.service.checkPasswordValidation(password: password)
+                
+                self.state.isMatchPassword = self.service.checkPasswordCheckMatch(password: password, passwordCheck: passwordCheck)
+            }
+            .store(in: &self.cancellables)
+        
+        let signUpState = Publishers.CombineLatest4(
+            self.$state.map(\.isEmailValid),
+            self.$state.map(\.isNicknameValid),
+            self.$state.map(\.isPasswordValid),
+            self.$state.map(\.isMatchPassword))
+            .removeDuplicates(by: ==)
+            .debounce(for: .milliseconds(350), scheduler: RunLoop.main)
+            .map { email, nickname , password, passwordCheck in
+                return email && nickname && password && passwordCheck
+            }
+        
+        let loginState = Publishers.CombineLatest(
+            self.$input.map(\.email),
+            self.$input.map(\.password))
+            .removeDuplicates(by: ==)
+            .map { email, password in
+                return !email.isEmpty && !password.isEmpty
+            }
+        
+        Publishers.CombineLatest(signUpState, loginState)
+            .removeDuplicates(by: ==)
+            .map { [weak self] signUpState, loginState in
+                guard let self else {
+                    return false
+                }
+                
+                return self.state.authType == .signUp ? signUpState : loginState
+            }
+            .sink { [weak self] canAuth in
+                guard let self else {
+                    return
+                }
+                
+                self.state.canAuth = canAuth
+            }
+            .store(in: &self.cancellables)
     }
     
     func action(_ action: Action) {
         switch action {
+        case .changeAuthType(let type):
+            self.changeAuthType(type)
         case .login:
             self.login()
         case .signUp:
             self.signUp()
+        case .reset:
+            self.reset()
         }
     }
     
+    private func changeAuthType(_ type: AuthWithEmailType) {
+        self.state.authType = type
+    }
+    
     private func login() {
-        
+        Task {
+            do {
+                let request = LoginWithEmailRequestEntity(email: self.input.email, password: self.input.password)
+                
+                let response = try await self.service.loginWithEmail(request: request)
+                
+                self.state.loginData = response
+            } catch let error as EstateErrorResponseEntity {
+                await MainActor.run {
+                    self.state.alertMessage = error.message
+                    self.state.showAlert = true
+                }
+            }
+        }
     }
     
     private func signUp() {
-        
+        Task {
+            do {
+                let request = SignUpWithEmailRequestEntity(email: self.input.email, password: self.input.password, nickname: self.input.nickname)
+                
+                let response = try await self.service.signUpWithEmail(request: request)
+                
+                self.state.loginData = response
+                
+            } catch let error as EstateErrorResponseEntity {
+                await MainActor.run {
+                    self.state.alertMessage = error.message
+                    self.state.showAlert = true
+                }
+            }
+        }
+    }
+    
+    private func reset() {
+        self.input = .init()
     }
 }
