28bb0afc107a8bbff53b2eabec573181c7c23384
Park Junwoo <ays99111@naver.com>
2025-05-23 16:28:29 +0900

    MainView 파일 추가

---
+++ Modungji/Feature/Main/View/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  Modungji
+//
+//  Created by 박준우 on 5/23/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("MainView")
+    }
+}
+
+#Preview {
+    MainView()
+}
