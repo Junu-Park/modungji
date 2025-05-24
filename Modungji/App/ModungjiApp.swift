951c2c039188bb7edbf0a5f2cc8d42ae07699b45
Park Junwoo <ays99111@naver.com>
2025-05-24 15:47:08 +0900

    ModungjiApp 파일 수정
    RootView 적용
    DIContainer 의존성 주입 적용

--- Modungji/App/ModungjiApp.swift
+++ Modungji/App/ModungjiApp.swift
@@ -7,48 +7,22 @@
 
 import SwiftUI
 
-import KakaoSDKAuth
 import KakaoSDKCommon
 import ModungjiSecret
 
 @main
 struct ModungjiApp: App {
-    @StateObject private var authViewModel: AuthViewModel
-    @StateObject private var authWithEmailViewModel: AuthWithEmailViewModel
+    private let diContainer = DIContainer.getDefaultDIContainer()
     
     init() {
+        // 카카오 SDK 초기화
         KakaoSDK.initSDK(appKey: ModungjiSecret.Kakao.key)
-        
-        let authRepository = AuthRepositoryImp(networkManager: NetworkManager(), kakaoManager: KakaoManager(), keychainManger: KeychainManager())
-        let authService = AuthServiceImp(
-            repository: authRepository
-        )
-        
-        self._authViewModel = StateObject(wrappedValue: AuthViewModel(service: authService))
-        self._authWithEmailViewModel = StateObject(wrappedValue: AuthWithEmailViewModel(service: authService))
     }
     
     var body: some Scene {
         WindowGroup {
-            NavigationStack {
-                if self.authViewModel.state.loginData == nil && self.authWithEmailViewModel.state.loginData == nil {
-                    AuthView()
-                        .onOpenURL { url in
-                            if AuthApi.isKakaoTalkLoginUrl(url) {
-                                guard AuthController.handleOpenUrl(url: url) else {
-                                    print("유효하지 않은 리다이렉트 URL")
-                                    return
-                                }
-                            } else {
-                                print("카카오톡이 아닌 다른 곳에서 리다이렉트된 URL")
-                            }
-                        }
-                } else {
-                    MainView()
-                }
-            }
-            .environmentObject(self.authViewModel)
-            .environmentObject(self.authWithEmailViewModel)
+            RootView()
+                .injectDependency(self.diContainer)
         }
     }
 }
