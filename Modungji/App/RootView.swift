b40b4116e9fbf56ede180d6c3ee4ed60cb92265f
Park Junwoo <ays99111@naver.com>
2025-05-24 15:41:14 +0900

    RootView 파일 생성
    App EntryPoint에서 뷰 분리

---
+++ Modungji/App/RootView.swift
@@ -0,0 +1,38 @@
+//
+//  RootView.swift
+//  Modungji
+//
+//  Created by 박준우 on 5/24/25.
+//
+
+import SwiftUI
+
+import KakaoSDKAuth
+
+struct RootView: View {
+    @EnvironmentObject var authState: AuthState
+    
+    var body: some View {
+        NavigationStack {
+            if self.authState.isLogin {
+                MainView()
+            } else {
+                AuthView()
+                    .onOpenURL { url in
+                        self.handleRedirectUrl(url)
+                    }
+            }
+        }
+    }
+    
+    private func handleRedirectUrl(_ url: URL) {
+        if AuthApi.isKakaoTalkLoginUrl(url) {
+            guard AuthController.handleOpenUrl(url: url) else {
+                print("유효하지 않은 리다이렉트 URL")
+                return
+            }
+        } else {
+            print("카카오톡이 아닌 다른 곳에서 리다이렉트된 URL")
+        }
+    }
+}
