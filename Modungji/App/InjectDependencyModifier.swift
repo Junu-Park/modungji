5f4bbe3549490ec15309afabfb1723999de895c7
Park Junwoo <ays99111@naver.com>
2025-05-24 15:32:10 +0900

    InjectDependencyModifier 파일 생성

---
+++ Modungji/App/InjectDependencyModifier.swift
@@ -0,0 +1,37 @@
+//
+//  InjectDependencyModifier.swift
+//  Modungji
+//
+//  Created by 박준우 on 5/24/25.
+//
+
+import SwiftUI
+
+private struct InjectDependencyModifier: ViewModifier {
+    let diContainer: DIContainer
+    let authState: AuthState = AuthState()
+    
+    func body(content: Content) -> some View {
+        content
+            .environment(\.DIContainer, self.diContainer)
+            .environmentObject(self.authState)
+            .environmentObject(
+                AuthViewModel(
+                    service: self.diContainer.service.authService,
+                    authState: self.authState
+                )
+            )
+            .environmentObject(
+                AuthWithEmailViewModel(
+                    service: self.diContainer.service.authService,
+                    authState: self.authState
+                )
+            )
+    }
+}
+
+extension View {
+    func injectDependency(_ diContainer: DIContainer) -> some View {
+        modifier(InjectDependencyModifier(diContainer: diContainer))
+    }
+}
