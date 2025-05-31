9e659b8072241faa7965a8678b5a145f8d3f9dfd
Park Junwoo <ays99111@naver.com>
2025-05-31 16:55:08 +0900

    NaverMapView, NaverMapViewModel 적용

--- Modungji/Feature/Map/View/NaverMapView.swift
+++ Modungji/Feature/Map/View/NaverMapView.swift
@@ -10,14 +10,34 @@ import SwiftUI
 import NMapsMap
 
 struct NaverMapView: UIViewRepresentable {
+    @ObservedObject var viewModel: NaverMapViewModel
     
     func makeUIView(context: Context) -> NMFNaverMapView {
-        let naverMap = NMFNaverMapView()
-        
-        return naverMap
+        return getNaverMapView()
     }
     
     func updateUIView(_ uiView: NMFNaverMapView, context: Context) {
         print(#function)
+    }
+    
+    private func getNaverMapView() -> NMFNaverMapView {
+        let naverMap = NMFNaverMapView()
+        
+        // 줌 컨트롤 UI 제거
+        naverMap.showZoomControls = false
+        
+        // 시작 위치 설정
+        let starCoord = NMGLatLng(
+            lat: self.viewModel.currentCoord.latitude,
+            lng: self.viewModel.currentCoord.longitude
+        )
+        let cameraUpdate = NMFCameraUpdate(scrollTo: starCoord)
+        cameraUpdate.animation = .fly
+        naverMap.mapView.moveCamera(cameraUpdate)
+        
+        // viewModel과 연결
+        self.viewModel.setNaverMapView(naverMap.mapView)
+        
+        return naverMap
     }
 }
