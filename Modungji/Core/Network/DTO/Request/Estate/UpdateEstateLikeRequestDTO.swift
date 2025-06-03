4310851ee20fba5e4655169d28f5c1f1ef260a88
Park Junwoo <ays99111@naver.com>
2025-06-03 16:45:45 +0900

    UpdateEstateLikeRequestDTO, Request DTO 구현

---
+++ Modungji/Core/Network/DTO/Request/Estate/UpdateEstateLikeRequestDTO.swift
@@ -0,0 +1,12 @@
+//
+//  UpdateEstateLikeRequestDTO.swift
+//  Modungji
+//
+//  Created by 박준우 on 6/3/25.
+//
+
+import Foundation
+
+struct UpdateEstateLikeRequestDTO: Encodable {
+    var like_status: Bool
+}
