36ac509246e92ad4872e17c4d276cfbc431bef90
Park Junwoo <ays99111@naver.com>
2025-06-05 21:02:00 +0900

    GeolocationEntity 수정
    Equatable 프로토콜 채택
    두 좌표 사이 실제 미터 거리 구하는 메서드 추가

--- Modungji/Feature/Map/Model/GeolocationEntity.swift
+++ Modungji/Feature/Map/Model/GeolocationEntity.swift
@@ -5,9 +5,16 @@
 //  Created by 박준우 on 5/31/25.
 //
 
-import Foundation
+import CoreLocation
 
-struct GeolocationEntity {
+struct GeolocationEntity: Equatable {
     var latitude: Double
     var longitude: Double
+    
+    func getMeterDistance(with target: GeolocationEntity) -> Int {
+        let standard = CLLocation(latitude: self.latitude, longitude: self.longitude)
+        let target = CLLocation(latitude: target.latitude, longitude: target.longitude)
+        
+        return Int(standard.distance(from: target))
+    }
 }
