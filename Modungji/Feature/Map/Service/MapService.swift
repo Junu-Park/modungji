76ddde54209205606561272be3520bc238da5fb6
Park Junwoo <ays99111@naver.com>
2025-06-05 19:27:44 +0900

    MapService, Map 관련 Service 계층 프로토콜 구현

---
+++ Modungji/Feature/Map/Service/MapService.swift
@@ -0,0 +1,13 @@
+//
+//  MapService.swift
+//  Modungji
+//
+//  Created by 박준우 on 6/5/25.
+//
+
+import Foundation
+
+protocol MapService {
+    func getEstateWithGeo(entity: GetEstateWithGeoRequestEntity) async throws -> [GetEstateWithGeoResponseEntity]
+    func getUserLocation() async throws
+}
