4ea7609245fcfc8f28958aeb71cf6ced0e6af467
Park Junwoo <ays99111@naver.com>
2025-07-13 12:17:12 +0900

    AppDelegate 클래스 final 키워드 추가

--- Modungji/App/AppDelegate.swift
+++ Modungji/App/AppDelegate.swift
@@ -10,7 +10,7 @@ import SwiftUI
 import FirebaseCore
 import FirebaseMessaging
 
-class AppDelegate: NSObject, UIApplicationDelegate {
+final class AppDelegate: NSObject, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
