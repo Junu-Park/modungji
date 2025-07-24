22573e53a9f85dba859b30b46ae5915bd1dd1fdd
Park Junwoo <ays99111@naver.com>
2025-07-24 15:27:30 +0900

    RefreshResponseEntity 구현

---
+++ Modungji/Feature/Auth/Model/RefreshResponseEntity.swift
@@ -0,0 +1,13 @@
+//
+//  RefreshResponseEntity.swift
+//  Modungji
+//
+//  Created by 박준우 on 7/24/25.
+//
+
+import Foundation
+
+struct RefreshResponseEntity {
+    let accessToken: String
+    let refreshToken: String
+}
