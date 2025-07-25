224ec99897c2f738c08a2127542d26772157f6b2
Park Junwoo <ays99111@naver.com>
2025-07-25 15:10:48 +0900

    BannerEstateResponseEntity Entity 생성

---
+++ Modungji/Feature/Main/Model/BannerEstateResponseEntity.swift
@@ -0,0 +1,16 @@
+//
+//  BannerEstateResponseEntity.swift
+//  Modungji
+//
+//  Created by 박준우 on 7/24/25.
+//
+
+import Foundation
+
+struct BannerEstateResponseEntity {
+    let estateId: String
+    let title: String
+    let introduction: String
+    let thumbnails: String
+    let geolocation: GeolocationEntity
+}
