a7abe10a52cbca6811c41506c50a56cfcb238fd5
Park Junwoo <ays99111@naver.com>
2025-08-04 16:08:09 +0900

    MainViewModel 구현

---
+++ Modungji/Feature/Main/ViewModel/MainViewModel.swift
@@ -0,0 +1,56 @@
+//
+//  MainViewModel.swift
+//  Modungji
+//
+//  Created by 박준우 on 7/24/25.
+//
+
+import Combine
+
+final class MainViewModel: ObservableObject {
+    struct State {
+        var bannerEstateList: [BannerEstateResponseEntity] = []
+        var showErrorAlert: Bool = false
+        var errorMessage: String = ""
+    }
+    
+    enum Action {
+        case initView
+    }
+    
+    @Published var state: State = State()
+    private var cancellables: Set<AnyCancellable> = []
+    private let service: MainService
+    
+    init(service: MainService) {
+        self.service = service
+    }
+    
+    private func transform() {
+        
+    }
+    
+    func action(_ action: Action) {
+        switch action {
+        case .initView:
+            self.initView()
+        }
+    }
+    
+    private func initView() {
+        Task {
+            do {
+                let result = try await self.service.getBannerEstate()
+                
+                await MainActor.run {
+                    self.state.bannerEstateList = result
+                }
+            } catch let error as EstateErrorResponseEntity {
+                await MainActor.run {
+                    self.state.errorMessage = error.message
+                    self.state.showErrorAlert = true
+                }
+            }
+        }
+    }
+}
