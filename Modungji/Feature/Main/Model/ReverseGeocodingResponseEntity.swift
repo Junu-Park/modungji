49d59930ca5c99f91f310cd3aff0cd8d00861e1e
Park Junwoo <ays99111@naver.com>
2025-08-06 14:39:01 +0900

    ReverseGeocodingResponseEntity 구현

---
+++ Modungji/Feature/Main/Model/ReverseGeocodingResponseEntity.swift
@@ -0,0 +1,15 @@
+//
+//  ReverseGeocodingResponseEntity.swift
+//  Modungji
+//
+//  Created by 박준우 on 8/6/25.
+//
+
+import Foundation
+
+struct ReverseGeocodingResponseEntity {
+    let area1: String
+    let area1Alias: String
+    let area2: String
+    let area3: String
+}
