aa6de4679707956b4d80535bfbba617ce24e8661
Park Junwoo <ays99111@naver.com>
2025-08-07 14:45:08 +0900

    NaverRouter Map 라우터 구현

---
+++ Modungji/Core/Network/Router/Naver/Map.swift
@@ -0,0 +1,68 @@
+//
+//  Map.swift
+//  Modungji
+//
+//  Created by 박준우 on 8/7/25.
+//
+
+import Foundation
+
+import Alamofire
+import ModungjiSecret
+
+extension NaverRouter {
+    enum Map: APIRouter {
+        case reverseGeocoding(coords: String)
+        case geocoding(query: String)
+        
+        var baseURL: URL? {
+            return URL(string: NaverRouter.baseURL)
+        }
+        
+        var path: String {
+            switch self {
+            case .reverseGeocoding(let coords):
+                return "map-reversegeocode/v2/gc\(coords)"
+            case .geocoding(let query):
+                return "map-geocode/v2/geocode\(query)"
+            }
+        }
+        
+        var method: HTTPMethod {
+            switch self {
+            default:
+                return .get
+            }
+        }
+        
+        var queryParameters: [String : Any]? {
+            switch self {
+            case .reverseGeocoding(let coords):
+                return [
+                    "coords": coords,
+                    "orders": "admcode",
+                    "output": "json"
+                ]
+            case .geocoding(let query):
+                return ["query": query]
+            }
+        }
+        
+        var body: Encodable? {
+            switch self {
+            default:
+                return nil
+            }
+        }
+        
+        var headers: HTTPHeaders {
+            var headers = HTTPHeaders()
+            
+            headers.add(name: "x-ncp-apigw-api-key-id", value: ModungjiSecret.Naver.clientID)
+            
+            headers.add(name: "x-ncp-apigw-api-key", value: ModungjiSecret.Naver.key)
+            
+            return headers
+        }
+    }
+}
---
+++ Modungji/Core/Network/Router/Naver/NaverRouter.swift
@@ -0,0 +1,12 @@
+//
+//  NaverRouter.swift
+//  Modungji
+//
+//  Created by 박준우 on 8/7/25.
+//
+
+import ModungjiSecret
+
+enum NaverRouter {
+    static let baseURL: String = ModungjiSecret.Naver.baseURL
+}
