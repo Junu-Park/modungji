8acc9543d3d2880b1db35a4152b96a451f9380aa
Park Junwoo <ays99111@naver.com>
2025-08-12 14:50:10 +0900

    UpdateEstateLikeResponseEntity 모델 구현

---
+++ Modungji/Feature/Detail/Model/UpdateEstateLikeResponseEntity.swift
@@ -0,0 +1,12 @@
+//
+//  UpdateEstateLikeResponseEntity.swift
+//  Modungji
+//
+//  Created by 박준우 on 8/12/25.
+//
+
+import Foundation
+
+struct UpdateEstateLikeResponseEntity {
+    let likeStatus: Bool
+}
