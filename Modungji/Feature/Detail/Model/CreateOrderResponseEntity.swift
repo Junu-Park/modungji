bcfef9b3c7c6a3615ab8543f783b65c92e847305
Park Junwoo <ays99111@naver.com>
2025-08-14 14:43:00 +0900

    CreateOrderResponse Entity 구현

---
+++ Modungji/Feature/Detail/Model/CreateOrderResponseEntity.swift
@@ -0,0 +1,16 @@
+//
+//  CreateOrderResponseEntity.swift
+//  Modungji
+//
+//  Created by 박준우 on 8/15/25.
+//
+
+import Foundation
+
+struct CreateOrderResponseEntity {
+    let orderID: String
+    let orderCode: String
+    let totalPrice: Int
+    let createdAt: String
+    let updatedAt: String
+}
