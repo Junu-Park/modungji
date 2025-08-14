4df6f2d3c4a9b505b0c3ad4e7fe5e806a9553819
Park Junwoo <ays99111@naver.com>
2025-08-14 14:39:09 +0900

    Order API 라우터 구현

---
+++ Modungji/Core/Network/Router/Estate/Order.swift
@@ -0,0 +1,65 @@
+//
+//  Order.swift
+//  Modungji
+//
+//  Created by 박준우 on 8/14/25.
+//
+
+import Foundation
+
+import Alamofire
+import ModungjiSecret
+
+extension EstateRouter {
+    enum Order: APIRouter {
+        case createOrder(body: CreateOrderRequestDTO)
+        case getOrderHistory
+        
+        var baseURL: URL? {
+            return URL(string: EstateRouter.baseURL)?.appendingPathComponent("v1/orders")
+        }
+        
+        var path: String {
+            return ""
+        }
+        
+        var method: HTTPMethod {
+            switch self {
+            case .createOrder:
+                return .post
+            case .getOrderHistory:
+                return .get
+            }
+        }
+        
+        var queryParameters: [String : Any]? {
+            return nil
+        }
+        
+        var body: Encodable? {
+            switch self {
+            case .createOrder(let dto):
+                return dto
+            case .getOrderHistory:
+                return nil
+            }
+        }
+        
+        var headers: HTTPHeaders {
+            var headers = HTTPHeaders()
+            
+            headers.add(name: "SeSACKey", value: ModungjiSecret.Estate.key)
+            
+            headers.add(name: "Content-Type", value: "application/json")
+            
+            do {
+                let token = try KeychainManager().getToken(tokenType: .accessToken)
+                headers.add(name: "Authorization", value: token)
+            } catch {
+                print(error)
+            }
+            
+            return headers
+        }
+    }
+}
