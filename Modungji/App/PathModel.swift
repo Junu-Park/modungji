1de9c850d70155f9af619814bcc7fc0fd5e800e5
Park Junwoo <ays99111@naver.com>
2025-08-16 18:49:01 +0900

    PathModel ViewModel 캐싱 로직 추가

--- Modungji/App/PathModel.swift
+++ Modungji/App/PathModel.swift
@@ -12,6 +12,8 @@ final class PathModel: ObservableObject {
     
     private let diContainer: DIContainer
     
+    private var viewModelList: [String: any ObservableObject] = [:]
+    
     init(diContainer: DIContainer) {
         self.diContainer = diContainer
     }
@@ -27,6 +29,7 @@ final class PathModel: ObservableObject {
     }
     
     func root() {
+        self.viewModelList.removeAll()
         self.path = NavigationPath()
     }
     
@@ -34,40 +37,117 @@ final class PathModel: ObservableObject {
     func build(_ path: PathType) -> some View {
         switch path {
         case .auth:
-            let viewModel = AuthViewModel(
-                service: self.diContainer.service.authService,
-                authState: self.diContainer.state.authState
-            )
+            let viewModel: AuthViewModel = {
+                if let vm = self.viewModelList["AuthViewModel"] as? AuthViewModel {
+                    return vm
+                } else {
+                    let vm = AuthViewModel(
+                        service: self.diContainer.service.authService,
+                        authState: self.diContainer.state.authState
+                    )
+                    self.viewModelList["AuthViewModel"] = vm
+                    
+                    return vm
+                }
+            }()
             
             AuthView(viewModel: viewModel)
         case .authWithEmail(let authType):
-            let viewModel = AuthWithEmailViewModel(
+            /*
+            let viewModel: AuthWithEmailViewModel = {
+                if let vm = self.viewModelList["AuthWithEmailViewModel"] as? AuthWithEmailViewModel {
+                    return vm
+                } else {
+                    let vm = AuthWithEmailViewModel(
+                        service: self.diContainer.service.authService,
+                        authState: self.diContainer.state.authState
+                    )
+                    self.viewModelList["AuthWithEmailViewModel"] = vm
+                    
+                    return vm
+                }
+            }()
+             */
+            let viewModel: AuthWithEmailViewModel = AuthWithEmailViewModel(
                 service: self.diContainer.service.authService,
                 authState: self.diContainer.state.authState
             )
             
             AuthWithEmailView(authType: authType, viewModel: viewModel)
         case .main:
-            let viewModel = MainViewModel(
-                service: self.diContainer.service.mainService
-            )
+            let viewModel: MainViewModel = {
+                if let vm = self.viewModelList["MainViewModel"] as? MainViewModel {
+                    return vm
+                } else {
+                    let vm = MainViewModel(
+                        service: self.diContainer.service.mainService
+                    )
+                    self.viewModelList["MainViewModel"] = vm
+                    
+                    return vm
+                }
+            }()
             
             MainView(viewModel: viewModel)
         case .map:
-            let viewModel = MapViewModel(
-                service: self.diContainer.service.mapService,
-                pathModel: self
-            )
+            let viewModel: MapViewModel = {
+                if let vm = self.viewModelList["MapViewModel"] as? MapViewModel {
+                    return vm
+                } else {
+                    let vm = MapViewModel(
+                        service: self.diContainer.service.mapService,
+                        pathModel: self
+                    )
+                    self.viewModelList["MapViewModel"] = vm
+                    
+                    return vm
+                }
+            }()
             
             MapView(viewModel: viewModel)
         case .detail(let estateID):
-            let viewModel = DetailViewModel(
-                estateID: estateID,
-                service: self.diContainer.service.detailService,
+            let viewModel: DetailViewModel = {
+                if let vm = self.viewModelList["DetailViewModel"] as? DetailViewModel, vm.state.detailData.estateID == estateID {
+                    return vm
+                } else {
+                    let vm = DetailViewModel(
+                        estateID: estateID,
+                        service: self.diContainer.service.detailService,
+                        pathModel: self
+                    )
+                    self.viewModelList["DetailViewModel"] = vm
+                    
+                    return vm
+                }
+            }()
+            
+            DetailView(viewModel: viewModel)
+        case .chat(let opponentID):
+            /*
+            let viewModel: ChatViewModel = {
+                if let vm = self.viewModelList["ChatViewModel"] as? ChatViewModel {
+                    return vm
+                } else {
+                    let vm = ChatViewModel(
+                        opponentID: opponentID,
+                        service: self.diContainer.service.chatService,
+                        pathModel: self
+                    )
+                    self.viewModelList["ChatViewModel"] = vm
+                    
+                    return vm
+                }
+            }()
+            */
+            let viewModel: ChatViewModel = ChatViewModel(
+                opponentID: opponentID,
+                service: self.diContainer.service.chatService,
                 pathModel: self
             )
             
-            DetailView(viewModel: viewModel)
+            ChatView(viewModel: viewModel)
+        case .chatRoomList:
+            ChatRoomListView()
         }
     }
 }
