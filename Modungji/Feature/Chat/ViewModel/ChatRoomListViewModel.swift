2afdb3d840315e135361f60204e3226977cd0252
Park Junwoo <ays99111@naver.com>
2025-08-23 16:36:17 +0900

    ChatRoomListViewModel 채팅방 Local DB 저장 관련 로직 추가

--- Modungji/Feature/Chat/ViewModel/ChatRoomListViewModel.swift
+++ Modungji/Feature/Chat/ViewModel/ChatRoomListViewModel.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+import RealmSwift
+
 final class ChatRoomListViewModel: ObservableObject {
     struct State {
         var chatRoomList: [ChatRoomResponseEntity] = []
@@ -27,6 +29,8 @@ final class ChatRoomListViewModel: ObservableObject {
     init(service: ChatService, pathModel: PathModel) {
         self.service = service
         self.pathModel = pathModel
+        
+        self.action(.fetchChatRoomList)
     }
     
     func action(_ action: Action) {
@@ -46,12 +50,111 @@ final class ChatRoomListViewModel: ObservableObject {
                 await MainActor.run {
                     self.state.chatRoomList = response
                 }
+                
+                self.saveChatRoomToRealm(response)
             } catch let error as EstateErrorResponseEntity {
                 await MainActor.run {
+                    self.state.chatRoomList = self.getChatRoomFromRealm()
+                    
                     self.state.errorMessage = error.message
+                    self.state.showErrorAlert = true
+                }
+            } catch {
+                await MainActor.run {
+                    self.state.chatRoomList = self.getChatRoomFromRealm()
+                    
+                    self.state.errorMessage = error.localizedDescription
                     self.state.showErrorAlert = true
                 }
             }
         }
+    }
+    
+    private func getChatRoomFromRealm() -> [ChatRoomResponseEntity] {
+        let realm = try! Realm()
+        
+        return realm.objects(ChatRoomRealmDTO.self).map { self.convertRealmDTOToEntity($0) }
+    }
+    
+    private func saveChatRoomToRealm(_ entities: [ChatRoomResponseEntity]) {
+        let realm = try! Realm()
+        
+        try! realm.write {
+            for entity in entities {
+                let userDTO = convertEntityToRealmDTO(entity.userData)
+                let opponentDTO = convertEntityToRealmDTO(entity.opponentUserData)
+                
+                realm.add(userDTO, update: .modified)
+                realm.add(opponentDTO, update: .modified)
+                
+                if realm.object(ofType: ChatRoomRealmDTO.self, forPrimaryKey: entity.roomID) == nil {
+                    realm.add(self.convertEntityToRealmDTO(entity))
+                }
+            }
+        }
+    }
+    
+    private func convertRealmDTOToEntity(_ dto: ChatRoomRealmDTO) -> ChatRoomResponseEntity {
+        return .init(
+            roomID: dto.id,
+            createdAt: dto.createdDate,
+            updatedAt: dto.updatedDate,
+            userData: self.convertRealmDTOToEntity(dto.user!),
+            opponentUserData: self.convertRealmDTOToEntity(dto.opponent!),
+            lastChat: {
+                guard let lastChat = dto.lastChat else {
+                    return nil
+                }
+                
+                return .init(
+                    chatID: lastChat.id,
+                    roomID: dto.id,
+                    content: lastChat.content,
+                    createdAt: lastChat.date,
+                    updatedAt: lastChat.date,
+                    sender: self.convertRealmDTOToEntity(lastChat.sender!),
+                    files: Array(lastChat.files)
+                )
+            }()
+        )
+    }
+    
+    private func convertEntityToRealmDTO(_ entity: ChatRoomResponseEntity) -> ChatRoomRealmDTO {
+        let roomDTO = ChatRoomRealmDTO()
+        roomDTO.id = entity.roomID
+        roomDTO.createdDate = entity.createdAt
+        roomDTO.user = self.convertEntityToRealmDTO(entity.userData)
+        roomDTO.opponent = self.convertEntityToRealmDTO(entity.opponentUserData)
+        
+        return roomDTO
+    }
+    
+    private func convertEntityToRealmDTO(_ entity: ChatResponseEntity) -> ChatRealmDTO {
+        let chatDTO = ChatRealmDTO()
+        chatDTO.id = entity.chatID
+        chatDTO.content = entity.content
+        chatDTO.sender = self.convertEntityToRealmDTO(entity.sender)
+        chatDTO.date = entity.createdAt
+        
+        return chatDTO
+    }
+    
+    private func convertEntityToRealmDTO(_ entity: UserEntity) -> UserRealmDTO {
+        let userDTO = UserRealmDTO()
+        userDTO.id = entity.userID
+        userDTO.nick = entity.nick
+        userDTO.profileImage = entity.profileImage
+        userDTO.introduction = entity.introduction
+        
+        return userDTO
+    }
+    
+    private func convertRealmDTOToEntity(_ dto: UserRealmDTO) -> UserEntity {
+        return .init(
+            userID: dto.id,
+            nick: dto.nick,
+            introduction: dto.introduction,
+            profileImage: dto.profileImage
+        )
     }
 }
