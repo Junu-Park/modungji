20a11fcd3a179acb4c509afb03904f103722ce80
Park Junwoo <ays99111@naver.com>
2025-08-29 12:21:20 +0900

    UploadFiles API DTO 구현

---
+++ Modungji/Core/Network/DTO/Request/Chat/UploadFilesRequestDTO.swift
@@ -0,0 +1,15 @@
+//
+//  UploadFilesRequestDTO.swift
+//  Modungji
+//
+//  Created by 박준우 on 8/31/25.
+//
+
+import Foundation
+
+struct UploadFilesRequestDTO {
+    let data: Data
+    let key: String
+    let name: String
+    let type: String
+}
---
+++ Modungji/Core/Network/DTO/Response/Chat/UploadFilesResponseDTO.swift
@@ -0,0 +1,12 @@
+//
+//  UploadFilesResponseDTO.swift
+//  Modungji
+//
+//  Created by 박준우 on 8/16/25.
+//
+
+import Foundation
+
+struct UploadFilesResponseDTO: Decodable {
+    let files: [String]
+}
