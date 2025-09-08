e371f4ac2138025567f6642237b76dacad8f8d92
Park Junwoo <ays99111@naver.com>
2025-09-08 13:51:02 +0900

    HotEstateResponseEntity 생성

---
+++ Modungji/Feature/Main/Model/HotEstateResponseEntity.swift
@@ -0,0 +1,22 @@
+//
+//  HotEstateResponseEntity.swift
+//  Modungji
+//
+//  Created by 박준우 on 9/9/25.
+//
+
+import Foundation
+
+struct HotEstateResponseEntity {
+    let estateID: String
+    let title: String
+    let introduction: String
+    let thumbnail: String
+    let geolocation: GeolocationEntity
+    var address: String = ""
+    let deposit: Int
+    let monthlyRent: Int
+    let area: Double
+    let likeCount: Int
+    let isRecommended: Bool
+}
