a68370eaac14d6dc248b63c2d65b7c08e4f0e54d
Park Junwoo <ays99111@naver.com>
2025-09-10 15:05:27 +0900

    TodayEstateTopicResponseEntity 생성

---
+++ Modungji/Feature/Main/Model/TodayEstateTopicResponseEntity.swift
@@ -0,0 +1,15 @@
+//
+//  TodayEstateTopicResponseEntity.swift
+//  Modungji
+//
+//  Created by 박준우 on 9/10/25.
+//
+
+import Foundation
+
+struct TodayEstateTopicResponseEntity {
+    let title: String
+    let content: String
+    let date: String
+    let link: String
+}
