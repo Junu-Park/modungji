d9b63176c36d85469e4a05396d5742707a073f99
Park Junwoo <ays99111@naver.com>
2025-09-12 21:36:11 +0900

    MapOptionType enum 구현

---
+++ Modungji/Feature/Map/Model/MapOptionType.swift
@@ -0,0 +1,15 @@
+//
+//  MapOptionType.swift
+//  Modungji
+//
+//  Created by 박준우 on 9/12/25.
+//
+
+import Foundation
+
+enum MapOptionType: String, CaseIterable {
+    case category = "종류"
+    case area = "평수"
+    case monthlyRent = "월세"
+    case deposit = "보증금"
+}
