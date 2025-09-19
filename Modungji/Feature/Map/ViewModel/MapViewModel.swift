9a548d48d4b2776057c35596e14485664f0ce89e
Park Junwoo <ays99111@naver.com>
2025-09-19 21:28:37 +0900

    MapViewModel Option 관련 필터링 로직 구현

--- Modungji/Feature/Map/ViewModel/MapViewModel.swift
+++ Modungji/Feature/Map/ViewModel/MapViewModel.swift
@@ -16,6 +16,9 @@ final class MapViewModel: ObservableObject {
         var searchQuery: String = ""
         var selectedOptionType: MapOptionType?
         var selectedCategory: EstateCategory?
+        var selectedMaxAreaLevel: Double = 0
+        var selectedMaxMonthlyRentLevel: Double = 12
+        var selectedMaxDepositLevel: Double = 22
         var centerLocation: GeolocationEntity = GeolocationEntity(latitude: 37.5666805, longitude: 126.9784147)
         var maxDistance: Int?
         var showCurrentLocationMarker: Bool = false
@@ -23,6 +26,7 @@ final class MapViewModel: ObservableObject {
         var showErrorAlert: Bool = false
         var errorMessage: String = ""
         var shouldMoveCamera: Bool = false
+        var shouldReloadData: Bool = false
     }
     
     enum Action {
@@ -31,6 +35,7 @@ final class MapViewModel: ObservableObject {
         case selectCategory(category: EstateCategory?)
         case moveCamera(entity: NaverMapEntity)
         case completeMoveCamera
+        case completeReloadData
         case tapCurrentLocationButton
         case tapEstate(estateID: String)
         case tapBackButton
@@ -52,15 +57,27 @@ final class MapViewModel: ObservableObject {
     }
     
     func transform() {
-        Publishers.CombineLatest3(
-            self.$state.map(\.selectedCategory),
+        Publishers.CombineLatest4(self.$state.map(\.selectedCategory), self.$state.map(\.selectedMaxAreaLevel), self.$state.map(\.selectedMaxDepositLevel), self.$state.map(\.selectedMaxMonthlyRentLevel))
+            .dropFirst()
+            .removeDuplicates(by: ==)
+            .debounce(for: .seconds(0.7), scheduler: RunLoop.main)
+            .sink { [weak self] _ in
+                guard let self else { return }
+                Task { @MainActor in
+                    self.state.filteredEstateList = await self.filteringEstateList()
+                    self.state.shouldReloadData = true
+                }
+            }
+            .store(in: &self.cancellables)
+        
+        Publishers.CombineLatest(
             self.$state.map(\.centerLocation),
             self.$state.map(\.maxDistance)
         )
         .dropFirst()
         .removeDuplicates(by: ==)
         .debounce(for: .seconds(0.5), scheduler: RunLoop.main)
-        .sink { [weak self] category, centerLocation, maxDistance  in
+        .sink { [weak self] centerLocation, maxDistance  in
             guard let self else { return }
             Task {
                 do {
@@ -78,11 +95,12 @@ final class MapViewModel: ObservableObject {
                     
                     self.estateList = list
                     
+                    let filtered = await self.filteringEstateList()
+                    
                     await MainActor.run {
-                        self.state.filteredEstateList = self.estateList.filter { entity in
-                            entity.category == self.state.selectedCategory?.rawValue
-                        }
+                        self.state.filteredEstateList = filtered
                         self.state.currentAddress = address
+                        self.state.shouldReloadData = true
                     }
                 } catch let error as EstateErrorResponseEntity {
                     await MainActor.run {
@@ -102,7 +120,7 @@ final class MapViewModel: ObservableObject {
         case .tapOption(let option):
             self.state.selectedOptionType = option
         case .selectCategory(let category):
-            self.selectCategory(category)
+            self.state.selectedCategory = category
         case .moveCamera(let entity):
             self.moveCamera(entity: entity)
         case .tapCurrentLocationButton:
@@ -114,13 +132,14 @@ final class MapViewModel: ObservableObject {
         case .search:
             self.search()
         case .completeMoveCamera:
-            self.state.shouldMoveCamera = false
+            Task { @MainActor in
+                self.state.shouldMoveCamera = false
+            }
+        case .completeReloadData:
+            Task { @MainActor in
+                self.state.shouldReloadData = false
+            }
         }
-    }
-    
-    private func selectCategory(_ category: EstateCategory?) {
-        self.state.selectedCategory = category
-        self.state.shouldMoveCamera = true
     }
     
     private func moveCamera(entity: NaverMapEntity) {
@@ -193,5 +212,53 @@ final class MapViewModel: ObservableObject {
         self.state.filteredEstateList.removeAll()
         self.state.shouldMoveCamera = true
         self.pathModel.pop()
+    }
+    
+    private func filteringEstateList() async -> [GetEstateWithGeoResponseEntity] {
+        return self.estateList.filter { entity in
+            let category = self.state.selectedCategory?.rawValue == nil ? true : entity.category == self.state.selectedCategory?.rawValue
+            
+            return category && entity.area >= self.convertAreaLevel() && entity.deposit <= self.convertDepositLevel() && entity.monthlyRent <= self.convertMonthlyRentLevel()
+        }
+    }
+    
+    private func convertAreaLevel() -> Double {
+        let result: Double
+        let lv = self.state.selectedMaxAreaLevel
+        
+        switch lv {
+        case 1...10: result = lv * 5
+        default: result = 1
+        }
+        return result
+    }
+    
+    private func convertMonthlyRentLevel() -> Int {
+        let result: Int
+        let lv = Int(self.state.selectedMaxMonthlyRentLevel)
+        
+        switch lv {
+        case 1...11: result = lv * 100_000
+        case 12...: result = Int.max
+        default: result = 0
+        }
+        return result
+    }
+    
+    private func convertDepositLevel() -> Int {
+        let result: Int
+        let lv = Int(self.state.selectedMaxDepositLevel)
+        
+        switch lv {
+        case 1...10: result = lv * 1_000_000
+        case 11...18: result = (Int(lv / 10) + Int(lv) % Int(10)) * 10_000_000
+        case 19: result = 100_000_000
+        case 20: result = 150_000_000
+        case 21: result = 200_000_000
+        case 22: result = Int.max
+        default: result = 0
+        }
+        
+        return result
     }
 }
