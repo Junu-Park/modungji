e7bb25b52a3fb9a564e66b1b4eb279725776c5d7
Park Junwoo <ays99111@naver.com>
2025-09-29 16:41:02 +0900

    GetSimilarEstates Response Entity 구현

---
+++ Modungji/Feature/Detail/Model/GetSimilarEstatesResponseEntity.swift
@@ -0,0 +1,12 @@
+//
+//  GetSimilarEstatesResponseEntity.swift
+//  Modungji
+//
+//  Created by 박준우 on 9/29/25.
+//
+
+import Foundation
+
+struct GetSimilarEstatesResponseEntity {
+    let data: [EstateResponseEntity]
+}
